6ac3ef1ad90a8e1401e27199927d0b61e9030037
sayyeone <iamadistyfa002.com>
2026-02-18 18:07:19 +0700

    testing-1

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig(({ mode }) => {
       port: 5173,
       proxy: {
         '/api': {
-          target: 'http://127.0.0.1:8000',
+          target: 'https://sistem-penghitungan-piring-menggunakan-rfid-production.up.railway.app',
           changeOrigin: true,
         }
       }
